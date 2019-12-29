e55263ce3d93b973d77a7ba058ce18d4dde00da4
Dahn <daniel.jahn@targomo.com>
2019-12-29 15:07:19 +0100

    Fix out of bounds intervalsList bug, clean code
    (1..12) includes its bounds and there indexing starts from 0, so 12 resulted
    in out of bounds error
    Also, there's no need to define interalsList each time interval() is called.

--- app/src/main/java/com/example/m6/MainActivity.kt
+++ app/src/main/java/com/example/m6/MainActivity.kt
@@ -16,6 +16,8 @@ import android.media.MediaPlayer
 
 
 class MainActivity : AppCompatActivity() {
+    private val intervalsList = listOf("m2", "M2", "m3", "M3", "P4", "d5", "P5", "m6", "M6", "m7", "M7", "P8")
+    //val map = mapOf("m2" to 1, "M2" to 2, "m3" to 3, "M3" to 4, "P4" to 5, "d5" to 6, "P5" to 7, "m6" to 8, "M6" to 9, "m7" to 10, "M7" to 11, "P8" to 12)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,17 +25,11 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun pozdravKundicko(view: View) {
-        Toast.makeText(this,"Jeste jednou cus kundicko",Toast.LENGTH_LONG).show()
-
-    }
-
     fun interval(view: View){
-        val intrvl = (1..12).random()
-        // print(intrvl)
-        //val map = mapOf("m2" to 1, "M2" to 2, "m3" to 3, "M3" to 4, "P4" to 5, "d5" to 6, "P5" to 7, "m6" to 8, "M6" to 9, "m7" to 10, "M7" to 11, "P8" to 12)
-        val intervalsList = listOf("m2", "M2", "m3", "M3", "P4", "d5", "P5", "m6", "M6", "m7", "M7", "P8")
-        val chosenOne = intervalsList.get(intrvl)
+        val intrvl = (0..11).random()
+        val chosenOne = intervalsList[intrvl]
+        val chosenPitch = (0..11).random()
+
         val chosenPitch = (30..50).random()
         playSound(chosenPitch)
         Toast.makeText(this,chosenOne, Toast.LENGTH_LONG).show()
