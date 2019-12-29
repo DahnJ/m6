eef4d8ead421c71deb17358fc2b5bd19c1ddfd8a
Daniel Jahn <dahnjahn@gmail.com>
2019-12-29 14:53:05 +0100

    Basic random note implementation

--- app/src/main/java/com/example/m6/MainActivity.kt
+++ app/src/main/java/com/example/m6/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-//import android.media.MediaPlayer
+import android.media.MediaPlayer
 //import kotlin.random
 //import android.app.Activity
 //import android.media.MediaPlayer
@@ -34,10 +34,19 @@ class MainActivity : AppCompatActivity() {
         //val map = mapOf("m2" to 1, "M2" to 2, "m3" to 3, "M3" to 4, "P4" to 5, "d5" to 6, "P5" to 7, "m6" to 8, "M6" to 9, "m7" to 10, "M7" to 11, "P8" to 12)
         val intervalsList = listOf("m2", "M2", "m3", "M3", "P4", "d5", "P5", "m6", "M6", "m7", "M7", "P8")
         val chosenOne = intervalsList.get(intrvl)
-        Toast.makeText(this,chosenOne.toString(),Toast.LENGTH_LONG).show()
+        val chosenPitch = (30..50).random()
+        playSound(chosenPitch)
+        Toast.makeText(this,chosenOne, Toast.LENGTH_LONG).show()
     }
 
 
+    fun playSound(pitch: Int) {
+        val filename = "p$pitch"
+        val resourceId = resources.getIdentifier(filename, "raw", this.applicationInfo.packageName)
+
+        val mediaPlayer: MediaPlayer? = MediaPlayer.create(this.applicationContext, resourceId)
+        mediaPlayer?.start()
+    }
 
 
 }
